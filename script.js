b4d1559ead223332b024b16c16348c788312de09
Agnes <agnesajema@gmail.com>
2024-03-29 14:09:26 +0300

    add changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //create a close button and append it to each list item
 let myNodeList = document.getElementsByTagName("LI");
 var i ;
-for(i =0; -i<myNodeList.length;i++){                          //looping
+for(i =0; i < myNodeList.length; i++){                          //looping
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -20,10 +20,10 @@ for(i = 0; i < close.length; i++) {
 //add checked when clicking on a list item
 let list = document.querySelector("ul");
 list.addEventListener("click",function (ev){
-if (ev.target.tagName === "LI"){
+if (ev.target.tagName === "LI") {
   ev.target.classList.toggle("checked");
 }
-})
+},false);
 
 //create a new  item when clicking on the "Add"button
 function newElement(){
@@ -36,8 +36,21 @@ function newElement(){
   }else{
     document.getElementById("myUL").appendChild(li);
   }
-}
 
+document .getElementById("taskInput").value="";
+let span = document.createElement("SPAN");
+let txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for (i=0;i< close.length;i++){
+  close[i].onclick = function(){
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+}
 
     
     
